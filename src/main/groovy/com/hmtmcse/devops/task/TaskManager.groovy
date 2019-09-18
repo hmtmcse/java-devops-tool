7fbdddc33eb007ae6be037da2b14c37ea5176f06
Touhid Mia <hmtm.cse@gmail.com>
2019-09-18 22:26:45 +0600

    Action Mapping

--- src/main/groovy/com/hmtmcse/devops/task/TaskManager.groovy
+++ src/main/groovy/com/hmtmcse/devops/task/TaskManager.groovy
@@ -1,4 +1,15 @@
 package com.hmtmcse.devops.task
 
+import com.hmtmcse.devops.report.StepBin
+import com.hmtmcse.devops.task.action.Task
+
 class TaskManager {
+
+    public Boolean execute(Task task){}
+
+    public Boolean executeFromJSON(String json){}
+
+    public Boolean executeFromJSONFile(String location){}
+
+    public StepBin getReport(){}
 }

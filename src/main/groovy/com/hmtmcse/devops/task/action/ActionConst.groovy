55a8aadbb2d341dd920209e7e2e9095a8af433f1
Touhid Mia <hmtm.cse@gmail.com>
2019-09-18 22:35:55 +0600

    Action Mapping

--- src/main/groovy/com/hmtmcse/devops/task/action/ActionConst.groovy
+++ src/main/groovy/com/hmtmcse/devops/task/action/ActionConst.groovy
@@ -2,8 +2,19 @@ package com.hmtmcse.devops.task.action
 
 class ActionConst {
 
-    public static final ACTION = [
-            COPY : "copy",
-    ]
+    public static final String COPY = "copy"
+    public static final String MOVE = "move"
+    public static final String REMOVE = "remove"
+    public static final String MAKE_DIR = "makeDir"
+    public static final String RENAME = "rename"
+    public static final String ARCHIVE = "archive"
+    public static final String ANSIBLE = "ansible"
+    public static final String LINUX_OPERATOR = "linuxOperator"
+    public static final String LINUX_SERVICE = "linuxService"
+    public static final String MY_SQL = "mySQL"
+    public static final String OS_PERMISSION = "osPermission"
+    public static final String FIND_REPLACE = "findReplace"
+    public static final String SE_LINUX = "seLinux"
+    public static final String SHELL_COMMAND = "shellCommand"
 
 }

f3fae6caaacd627174c8168ba3a57e138d251607
Touhid Mia <touhid@bitmascot.com>
2019-12-17 16:32:03 +0600

    DevOps Tools

--- src/main/java/com/hmtmcse/devops/plugin/copy/CopyOption.java
+++ src/main/java/com/hmtmcse/devops/plugin/copy/CopyOption.java
@@ -1,6 +1,7 @@
 package com.hmtmcse.devops.plugin.copy;
 
 public class CopyOption {
+   public Boolean removeIfExist = true;
    public Boolean replaceExisting = true;
    public Boolean copyAttributes = true;
    public Boolean noFollowLinks = true;

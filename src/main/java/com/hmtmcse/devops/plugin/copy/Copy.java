3074ccd0ecd78faf19666185734ab527e8feef5e
Touhid Mia <touhid@bitmascot.com>
2019-12-30 09:37:41 +0600

    DevOps Tools

--- src/main/java/com/hmtmcse/devops/plugin/copy/Copy.java
+++ src/main/java/com/hmtmcse/devops/plugin/copy/Copy.java
@@ -2,5 +2,19 @@ package com.hmtmcse.devops.plugin.copy;
 
 public class Copy {
 
+    public String source;
+    public String target;
     public CopyOption options = new CopyOption();
+
+
+    public Copy setSource(String source) {
+        this.source = source;
+        return this;
+    }
+
+
+    public Copy setTarget(String target) {
+        this.target = target;
+        return this;
+    }
 }
--- src/main/java/com/hmtmcse/devops/plugin/copy/CopyOption.java
+++ src/main/java/com/hmtmcse/devops/plugin/copy/CopyOption.java
@@ -1,8 +1,10 @@
 package com.hmtmcse.devops.plugin.copy;
 
 public class CopyOption {
+
    public Boolean removeIfExist = true;
    public Boolean replaceExisting = true;
    public Boolean copyAttributes = true;
    public Boolean noFollowLinks = true;
+
 }

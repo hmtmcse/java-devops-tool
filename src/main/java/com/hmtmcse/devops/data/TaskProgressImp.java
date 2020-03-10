98697eefb39ea88d1335557ea67b849368a1d269
Touhid Mia <touhid@bitmascot.com>
2020-03-10 17:46:46 +0600

    Update various issues

--- src/main/java/com/hmtmcse/devops/data/TaskProgressImp.java
+++ src/main/java/com/hmtmcse/devops/data/TaskProgressImp.java
@@ -2,6 +2,8 @@ package com.hmtmcse.devops.data;
 
 import com.hmtmcse.devops.system.skeleton.TaskProgress;
 
+import java.util.LinkedHashMap;
+
 public class TaskProgressImp implements TaskProgress {
 
     @Override
@@ -11,6 +13,11 @@ public class TaskProgressImp implements TaskProgress {
 
     @Override
     public void message(String message) {
+
+    }
+
+    @Override
+    public void message(LinkedHashMap<String, Object> data, String message, String status) {
 
     }
 }
--- src/main/java/com/hmtmcse/devops/system/skeleton/TaskProgress.java
+++ src/main/java/com/hmtmcse/devops/system/skeleton/TaskProgress.java
@@ -3,7 +3,10 @@ package com.hmtmcse.devops.system.skeleton;
 import com.hmtmcse.devops.data.TaskReport;
 import com.hmtmcse.devops.system.common.DevOpsException;
 
+import java.util.LinkedHashMap;
+
 public interface TaskProgress {
     public void progress(TaskReport taskReport);
     public void message(String message);
+    public void message(LinkedHashMap<String, Object> data, String message, String status);
 }
